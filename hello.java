636d44059db5f94162af87a8aa35549f407ce1c0
hayatul11 <hayatulemon340@gmail.com>
2021-01-22 15:07:00 +0600

    Added a new method called display to hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,10 @@
 public class hello{
     public static void main(String[]args){
         System.out.printIn("Vs code is awesome..");
+
+        greeting("Hayatul");
+    }
+    public static void greeting(String name){
+        System.out.printIn("Welcome". name);
     }
 }
